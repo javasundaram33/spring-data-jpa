4c1444de71cb16f5e39c34e534c3ed975fb08f03
javasundaram33 <javasundaram33@gmail.com>
2022-06-01 13:50:50 +0800

    Implemented course and course_material

---
+++ spring-data-jpa/src/main/java/com/javasundaram/spring/data/jpa/repository/CourseMaterialRepository.java
@@ -0,0 +1,9 @@
+package com.javasundaram.spring.data.jpa.repository;
+
+import com.javasundaram.spring.data.jpa.entity.CourseMaterial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseMaterialRepository extends JpaRepository<CourseMaterial, Long> {
+}
---
+++ spring-data-jpa/src/test/java/com/javasundaram/spring/data/jpa/repository/CourseMaterialRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.javasundaram.spring.data.jpa.repository;
+
+import com.javasundaram.spring.data.jpa.entity.Course;
+import com.javasundaram.spring.data.jpa.entity.CourseMaterial;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseMaterialRepositoryTest {
+
+    @Autowired
+    CourseMaterialRepository repository;
+
+    @Test
+    public void saveCourseMaterial(){
+        Course c = Course.builder()
+                .courseName("Java")
+                .credit(6)
+                .build();
+        CourseMaterial cm = CourseMaterial.builder()
+                .url("www.google.com")
+                .course(c)
+                .build();
+        repository.save(cm);
+    }
+
+}
